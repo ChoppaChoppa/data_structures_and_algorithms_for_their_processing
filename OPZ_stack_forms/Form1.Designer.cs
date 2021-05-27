2eb37c8b10683d85b851042acdcbf9fe60510b86
mora <gopnik0770@mail.ru>
2021-05-27 23:23:49 +0300

    опз

---
+++ OPZ_stack_forms/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace OPZ_stack_forms
+{
+    partial class FormMora
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Create = new System.Windows.Forms.Button();
+            this.Input = new System.Windows.Forms.TextBox();
+            this.outOpz = new System.Windows.Forms.TextBox();
+            this.OutAns = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Create
+            // 
+            this.Create.Location = new System.Drawing.Point(326, 90);
+            this.Create.Name = "Create";
+            this.Create.Size = new System.Drawing.Size(129, 43);
+            this.Create.TabIndex = 0;
+            this.Create.Text = "create";
+            this.Create.UseVisualStyleBackColor = true;
+            this.Create.Click += new System.EventHandler(this.Create_Click);
+            // 
+            // Input
+            // 
+            this.Input.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Input.Location = new System.Drawing.Point(12, 28);
+            this.Input.Multiline = true;
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(776, 56);
+            this.Input.TabIndex = 1;
+            // 
+            // outOpz
+            // 
+            this.outOpz.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.outOpz.Location = new System.Drawing.Point(12, 139);
+            this.outOpz.Multiline = true;
+            this.outOpz.Name = "outOpz";
+            this.outOpz.Size = new System.Drawing.Size(776, 56);
+            this.outOpz.TabIndex = 2;
+            // 
+            // OutAns
+            // 
+            this.OutAns.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.OutAns.Location = new System.Drawing.Point(12, 222);
+            this.OutAns.Multiline = true;
+            this.OutAns.Name = "OutAns";
+            this.OutAns.Size = new System.Drawing.Size(776, 56);
+            this.OutAns.TabIndex = 3;
+            // 
+            // FormMora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.OutAns);
+            this.Controls.Add(this.outOpz);
+            this.Controls.Add(this.Input);
+            this.Controls.Add(this.Create);
+            this.Name = "FormMora";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Create;
+        private System.Windows.Forms.TextBox Input;
+        private System.Windows.Forms.TextBox outOpz;
+        private System.Windows.Forms.TextBox OutAns;
+    }
+}
+
---
+++ OPZ_stack_forms/OPZ.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPZ_stack_forms
+{
+    class OPZ
+    {
+        private string _inputStr;
+        private string _outStr = "";//строка для вывода ответа опз
+
+        private bool check = true;
+
+        private Stack<char> stOperation = new Stack<char>();//Стэк для перевода в опз
+        private Stack<double> stNum = new Stack<double>();//Стэк для ответа
+
+        private List<string> listOutStr = new List<string>();
+
+        public string CreateOPZ(string inputStr)//преобразование в опз
+        {
+            _inputStr = inputStr.Replace(" ", ""); // удаляем пробелы из примера
+
+            foreach (var el in _inputStr)//перебор всех элементов в тексте примера
+            {
+                bool cont = false;
+                if (Char.IsNumber(el))
+                { //если элемент цифра добавляем сразу к ответу
+                    _outStr += el;
+                    continue;
+                }  
+
+                else if (Char.IsLetter(el)) { _outStr += el; check = false; continue; }
+
+                listOutStr.Add(_outStr);
+                _outStr = "";
+                cont = true;
+                if(cont)
+                {
+                    switch (el)
+                    {
+                        case '(': //открывающую скобку всегда добавляем в стэк
+                            stOperation.Push(el);
+                            break;
+
+                        case '+':
+                            if (stOperation.Count == 0) stOperation.Push(el); //если стэк пусть добавляем в него
+
+                            else if (stOperation.Peek() == '-' || stOperation.Peek() == '+' || stOperation.Peek() == '*' || stOperation.Peek() == '/' || stOperation.Peek() == '^' || stOperation.Peek() == '(') // если в стэке лежит один из этих операций мы добавляем их к ответу пока стэк не закончится или не встрети открывающую скобку, после данный знак добавляем в стэк
+                            {
+                                while (stOperation.Count != 0 && (stOperation.Peek() == '-' || stOperation.Peek() == '+' || stOperation.Peek() == '*' || stOperation.Peek() == '/' || stOperation.Peek() == '^')) listOutStr.Add( stOperation.Pop().ToString());
+
+                                stOperation.Push(el);
+                            }
+                            break;
+
+                        case '-':
+                            if (stOperation.Count == 0) stOperation.Push(el); //если стэк пусть добавляем в него
+
+                            else if (stOperation.Peek() == '-' || stOperation.Peek() == '+' || stOperation.Peek() == '*' || stOperation.Peek() == '/' || stOperation.Peek() == '^' || stOperation.Peek() == '(') // если в стэке лежит один из этих операций мы добавляем их к ответу пока стэк не закончится или не встрети открывающую скобку, после данный знак добавляем в стэк
+                            {
+                                while (stOperation.Count != 0 && (stOperation.Peek() == '-' || stOperation.Peek() == '+' || stOperation.Peek() == '*' || stOperation.Peek() == '/' || stOperation.Peek() == '^')) listOutStr.Add(stOperation.Pop().ToString());
+                                stOperation.Push(el);
+                            }
+                            break;
+
+                        case '*':
+                            if (stOperation.Count == 0) stOperation.Push(el); //если стэк пусть добавляем в него
+
+                            else if (stOperation.Peek() == '-' || stOperation.Peek() == '+' || stOperation.Peek() == '(') stOperation.Push(el); //умножение и деление может добавляться поверх + и -, из стэка удаляется операция только если встретиться умножение с умноением, так же с делением, + и - удаляют из стэка все кроме (. возведение в степень не удаляет ничего
+
+                            else if (stOperation.Peek() == '*' || stOperation.Peek() == '/')
+                            {
+                                listOutStr.Add(stOperation.Pop().ToString());
+                                stOperation.Push(el);
+                            }
+
+                            break;
+
+                        case '/':
+                            if (stOperation.Count == 0) stOperation.Push(el);
+
+                            else if (stOperation.Peek() == '-' || stOperation.Peek() == '+' || stOperation.Peek() == '(') stOperation.Push(el);
+
+                            else if (stOperation.Peek() == '*' || stOperation.Peek() == '/')
+                            {
+                                listOutStr.Add(stOperation.Pop().ToString());
+                                stOperation.Push(el);
+                            }
+
+                            break;
+
+                        case '^':
+                            if (stOperation.Count == 0) stOperation.Push(el);
+
+                            else stOperation.Push(el);
+
+                            break;
+                        case ')':// закрывающая скобка удаляет все из стэка пока не встретит открывающую
+                            while (stOperation.Peek() != '(') listOutStr.Add(stOperation.Pop().ToString());
+
+                            stOperation.Pop();
+
+                            break;
+
+                    }
+                }
+
+            }
+
+            
+            if(_outStr.Length != 0)
+            {
+                listOutStr.Add(_outStr);
+            }
+
+            if (stOperation.Count != 0) //если в стэке что то осталось добавляем их к ответу
+            {
+                while (stOperation.Count != 0)
+                {
+                    listOutStr.Add(stOperation.Pop().ToString());
+                }
+            }
+            if(stOperation.Count == 0)
+            {
+                _outStr = "";
+                for(int i = 0; i < listOutStr.Count; i++)
+                {
+                    _outStr += listOutStr[i];
+                }
+            }
+
+            return _outStr;
+            //CalculationOPZ(_outStr);
+        }
+
+        public string CalculationOPZ()//высчитывание ответа через опз
+        {
+            _inputStr = "";
+            if (check)
+            {
+                
+
+                double pushEl;
+                double pushSum; //ложиться ответ операции над двумя последними числами из стэка
+                double num2;
+                double num1;//в num1 num2 кладутся 2 последних числа из стэка для правильной операции над ними(нпример, если изначально пример был 3-2, то в стэке он будет лежать как 2 и 3, и при операции получится 2 - 3, что не верно)
+                string num = "";
+                
+                foreach (string el in listOutStr)
+                {
+
+                    if (double.TryParse(el, out pushEl)) { stNum.Push(pushEl); }
+
+                    else
+                    {
+                        switch (el) //если элемент операция, мы остаем последние 2 числа из стэка производим операцию и ответ кладем обратно в стэк.
+                        {
+                            case "+":
+                                num2 = stNum.Pop();
+                                num1 = stNum.Pop();
+                                pushSum = num1 + num2;
+                                stNum.Push(pushSum);
+                                Console.WriteLine("operation: +, sum: {0}", pushSum);
+                                break;
+
+                            case "-":
+                                num2 = stNum.Pop();
+                                num1 = stNum.Pop();
+                                pushSum = num1 - num2;
+                                stNum.Push(pushSum);
+                                Console.WriteLine("operation: -, sum: {0}", pushSum);
+                                break;
+
+                            case "*":
+                                num2 = stNum.Pop();
+                                num1 = stNum.Pop();
+                                pushSum = num1 * num2;
+                                stNum.Push(pushSum);
+                                Console.WriteLine("operation: *, sum: {0}", pushSum);
+                                break;
+
+                            case "/":
+                                num2 = stNum.Pop();
+                                num1 = stNum.Pop();
+                                pushSum = num1 / num2;
+                                stNum.Push(pushSum);
+                                Console.WriteLine("operation: /, sum: {0}", pushSum);
+                                break;
+
+                            case "^":
+                                num2 = stNum.Pop();
+                                num1 = stNum.Pop();
+                                pushSum = Convert.ToInt32(Math.Pow(num1, num2));
+                                stNum.Push(pushSum);
+                                Console.WriteLine("operation: ^, sum: {0}", pushSum);
+                                break;
+                        }
+                    }
+                }
+
+                string ans = stNum.Pop().ToString(); //в конце в стэке остается один элемент, его мы добавляем в ans
+
+                return ans;
+            }
+
+            check = true;
+            return _inputStr;
+        }
+    }
+}
